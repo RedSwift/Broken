e362c028c8cfde0b2a9af05e65a0f8533d0b1b61
Dominic <jxdlam@gmail.com>
2016-06-09 11:20:47 +0800

    changed bricks into object, created second brick hardcoded - need resolve clearRect

--- broken.js
+++ broken.js
@@ -1,46 +1,43 @@
 // declarations
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-var moveHeight = 10;
 var movingHeight = 10;
-var brickHeight = 70;
-var startWidth = canvas.width / 2;
+var moveY = 10;
+
+var bricks = {
+  color: 'red',
+  widthX: canvas.width / 2,
+  heightY: 10,
+  brickWidth: 24,
+  brickHeight: 24,
+  draw: function () {
+    for (var i = 0; i < 60; i += 20) {
+      ctx.fillStyle = this.color;
+      ctx.fillRect(this.widthX, this.heightY + i, this.brickWidth, this.brickHeight);
+    }
+  }
+};
+
 // -------------------logic--------------------
 // Draw bricks function
 function drawBricks () {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawRedBricks();
+  ctx.clearRect(bricks.widthX, bricks.heightY - 60, 24, 70);
   // collisionDetection
-  if (moveHeight + brickHeight > canvas.height) {
-    movingHeight = movingHeight - 70;
+  if (bricks.heightY + 70 > canvas.height) {
+    moveY = bricks.heightY - 70;
+    var newY = moveY;
+    moveY = 10;
+    bricks.heightY = 10;
   } else {
-    moveHeight += movingHeight;
+    bricks.heightY += moveY;
+  }
+  bricks.draw();
+  // loop and draw old bricks
+  if (newY) {
+      ctx.fillStyle = 'red';
+      ctx.fillRect(canvas.width / 2, newY, 24, 60);
+      console.log('this is working');
   }
 }
 
-// function for drawing 3 bricks of the same color
-function drawRedBricks () {
-  for (var i = 0; i < 60; i += 20) {
-    ctx.beginPath();
-    ctx.rect(startWidth, moveHeight + i, 20, 20);
-    ctx.fillStyle = 'red';
-    ctx.fill();
-    ctx.stroke();
-    ctx.closePath();
-  }
-}
-
-// setInterval(drawBricks, 100);
-var block = {
-  blockHeight: 20,
-  blockWidth: 20
-};
-var gridColumnCount = 10;
-var gridRowCount = 20;
-var grid = [];
-for (var i = 0; i < gridColumnCount; i++) {
-  grid[i] = [];
-  for (var r = 0; r < gridRowCount; r++) {
-    grid[i][r] = block;
-  }
-}
+setInterval(drawBricks, 100);
